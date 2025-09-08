daf758a056adbdc794b679d465bff12831ecc1fb
unknown <cerveraralph6@gmail.com>
2025-09-08 19:50:51 +0800

    code update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
   const hamburger = document.querySelector(".hamburger");
   const navLinks = document.getElementById("nav-links");
 
-  // Toggle when clicking the hamburger
+ 
   hamburger.addEventListener("click", function () {
     navLinks.classList.toggle("show");
   });
 
-  // Close when clicking anywhere else
+
   document.addEventListener("click", function (event) {
     if (!hamburger.contains(event.target) && !navLinks.contains(event.target)) {
       navLinks.classList.remove("show");
